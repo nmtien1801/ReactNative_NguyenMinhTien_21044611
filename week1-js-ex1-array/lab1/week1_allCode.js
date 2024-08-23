88d7691d80e08dacea9448741a8a16f828529a08
nmtien1801 <nickname1801@gmail.com>
2024-08-23 08:41:42 +0700

    done challegen allcoding 3

--- week1-js-ex1-array/lab1/week1_allCode.js
+++ week1-js-ex1-array/lab1/week1_allCode.js
@@ -102,3 +102,148 @@ if (markHigherBMI2) {
 }
 
 console.log("--------------------------------------------------");
+
+/*
+Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each 
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, 
+and print it to the console. Don't forget that there can be a draw, so test for that 
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
+team only wins if it has a higher score than the other team, and the same time a 
+score of at least 100 points. Hint: Use a logical operator to test for minimum 
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
+both teams have the same score and both have a score greater or equal 100 
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+*/
+
+// 1. Calculate the average score for each team
+console.log("B1");
+let dolphinsAverageScore1 = (96 + 108 + 89) / 3;
+let koalasAverageScore1 = (88 + 91 + 110) / 3;
+
+let dolphinsAverageScore2 = (97 + 112 + 101) / 3;
+let koalasAverageScore2 = (109 + 95 + 123) / 3;
+
+let dolphinsAverageScore3 = (97 + 112 + 101) / 3;
+let koalasAverageScore3 = (109 + 95 + 106) / 3;
+
+// in ra
+console.log(`Dolphins average score1: ${dolphinsAverageScore1}`);
+console.log(`Koalas average score1: ${koalasAverageScore1}`);
+
+console.log(`Dolphins average score2: ${dolphinsAverageScore2}`);
+console.log(`Koalas average score2: ${koalasAverageScore2}`);
+
+console.log(`Dolphins average score3: ${dolphinsAverageScore3}`);
+console.log(`Koalas average score3: ${koalasAverageScore3}`);
+
+console.log("B2");
+// 2. Compare the team's average scores to determine the winner of the competition, and print it to the console
+function determineWinner(dolphinsAverageScore, koalasAverageScore) {
+    if (dolphinsAverageScore > koalasAverageScore) {
+        return `Dolphins win (${dolphinsAverageScore} vs. ${koalasAverageScore})`;
+    } else if (koalasAverageScore > dolphinsAverageScore) {
+        return `Koalas win (${koalasAverageScore} vs. ${dolphinsAverageScore})`;
+    } else {
+        return `It's a draw (${dolphinsAverageScore} vs. ${koalasAverageScore})`;
+    }
+}
+
+console.log(determineWinner(dolphinsAverageScore1, koalasAverageScore1));
+console.log(determineWinner(dolphinsAverageScore2, koalasAverageScore2));
+console.log(determineWinner(dolphinsAverageScore3, koalasAverageScore3));
+
+console.log("B3");
+
+// 3. Bonus 1: Include a requirement for a minimum score of 100
+function determineWinnerWithMinimumScore(
+    dolphinsAverageScore,
+    koalasAverageScore
+) {
+    if (
+        dolphinsAverageScore > koalasAverageScore &&
+        dolphinsAverageScore >= 100
+    ) {
+        return `Dolphins win (${dolphinsAverageScore} vs. ${koalasAverageScore})`;
+    } else if (
+        koalasAverageScore > dolphinsAverageScore &&
+        koalasAverageScore >= 100
+    ) {
+        return `Koalas win (${koalasAverageScore} vs. ${dolphinsAverageScore})`;
+    } else {
+        return `No team wins the trophy`;
+    }
+}
+
+console.log(
+    determineWinnerWithMinimumScore(dolphinsAverageScore1, koalasAverageScore1)
+);
+
+console.log(
+    determineWinnerWithMinimumScore(dolphinsAverageScore2, koalasAverageScore2)
+);
+
+console.log(
+    determineWinnerWithMinimumScore(dolphinsAverageScore3, koalasAverageScore3)
+);
+
+console.log("B4");
+
+// 4. Bonus 2: Minimum score also applies to a draw
+
+function determineWinnerWithMinimumScoreAndDraw(
+    dolphinsAverageScore,
+    koalasAverageScore
+) {
+    if (
+        dolphinsAverageScore > koalasAverageScore &&
+        dolphinsAverageScore >= 100
+    ) {
+        return `Dolphins win (${dolphinsAverageScore} vs. ${koalasAverageScore})`;
+    } else if (
+        koalasAverageScore > dolphinsAverageScore &&
+        koalasAverageScore >= 100
+    ) {
+        return `Koalas win (${koalasAverageScore} vs. ${dolphinsAverageScore})`;
+    } else if (
+        dolphinsAverageScore === koalasAverageScore &&
+        dolphinsAverageScore >= 100 &&
+        koalasAverageScore >= 100
+    ) {
+        return `It's a draw (${dolphinsAverageScore} vs. ${koalasAverageScore})`;
+    } else {
+        return `No team wins the trophy`;
+    }
+}
+
+console.log(
+    determineWinnerWithMinimumScoreAndDraw(
+        dolphinsAverageScore1,
+        koalasAverageScore1
+    )
+);
+
+console.log(
+    determineWinnerWithMinimumScoreAndDraw(
+        dolphinsAverageScore2,
+        koalasAverageScore2
+    )
+);
+
+console.log(
+    determineWinnerWithMinimumScoreAndDraw(
+        dolphinsAverageScore3,
+        koalasAverageScore3
+    )
+);
+
+console.log("--------------------------------------------------");
