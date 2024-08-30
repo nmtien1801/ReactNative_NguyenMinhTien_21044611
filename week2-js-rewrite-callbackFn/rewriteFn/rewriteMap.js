4aa6dad00a4c185410f3d672eb6a989ec50968e7
H51M14\Student <Student@gmail.com>
2024-08-30 08:26:57 +0700

    done: rewrite Map function

--- week2-js-rewrite-callbackFn/rewriteFn/rewriteMap.js
+++ week2-js-rewrite-callbackFn/rewriteFn/rewriteMap.js
@@ -2,10 +2,15 @@
 
 let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+Array.prototype.mapFn = function(callback) {
+    let result = [];
 
+    for (let i = 0; i < this.length; i++) {
+        result.push(callback(this[i]));
+    }
+    return result;
+}
 
-let rs1 = arr1.map(function(item) {
-    return item * 2;
-});
+let rs1 = arr1.mapFn((item) => item * 2);
 
 console.log(rs1);
