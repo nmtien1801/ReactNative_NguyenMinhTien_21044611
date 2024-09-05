656352d75acfb19d9f27144e0ade1834ee6d9f2e
nmtien1801 <nickname1801@gmail.com>
2024-09-05 16:28:48 +0700

    done chalenge coding 2 lab2

---
+++ week2-js-rewrite-callbackFn/lab2/coding_challenge2.js
@@ -0,0 +1,40 @@
+/* 
+      Steven is still building his tip calculator, using the same rules as before: Tip 15% of
+      the bill if the bill value is between 50 and 300, and if the value is different, the tip is
+      20%.
+
+      Your tasks:
+      1. Write a function 'calcTip' that takes any bill value as an input and returns
+      the corresponding tip, calculated based on the rules above (you can check out
+      the code from first tip calculator challenge if you need to). Use the function
+      type you like the most. Test the function using a bill value of 100
+      2. And now let's use arrays! So create an array 'bills' containing the test data
+      below
+      3. Create an array 'tips' containing the tip value for each bill, calculated from
+      the function you created before
+      4. Bonus: Create an array 'total' containing the total values, so the bill + tip
+      
+      Test data: 125, 555 and 44
+
+      Hint: Remember that an array needs a value in each position, and that value can
+      actually be the returned value of a function! So you can just call a function as array
+      values (so don't store the tip values in separate variables first, but right in the new
+      array) 
+*/
+
+// 1. Hàm tính tiền tip
+const calcTip = (bill) =>
+  bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+// Test data: 125, 555 and 44
+// 2. Mảng chứa các hóa đơn
+let bills = [125, 555, 44];
+
+// 3. Mảng chứa các tiền tip
+let arrTips = bills.map((bill) => calcTip(bill));
+
+// 4. Mảng chứa tổng tiền
+let total = bills.map((bill, index) => bill + arrTips[index]);
+
+console.log(`tip arr: ${arrTips}`);
+console.log(`total arr: ${total}`);
