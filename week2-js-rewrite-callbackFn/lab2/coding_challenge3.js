273622a95fe21ce051db9963b5c6f00526eb9079
nmtien1801 <nickname1801@gmail.com>
2024-09-05 16:43:51 +0700

    done allCoding challenge 3 lab2

---
+++ week2-js-rewrite-callbackFn/lab2/coding_challenge3.js
@@ -0,0 +1,60 @@
+/*
+      Coding Challenge #3
+      Let's go back to Mark and John comparing their BMIs! This time, let's use objects to
+      implement the calculations! Remember: BMI = mass / height ** 2 = mass
+      / (height * height) (mass in kg and height in meter)
+      
+      Your tasks:
+      1. For each of them, create an object with properties for their full name, mass, and
+      height (Mark Miller and John Smith)
+      2. Create a 'calcBMI' method on each object to calculate the BMI (the same
+      method on both objects). Store the BMI value to a property, and also return it
+      from the method
+      3. Log to the console who has the higher BMI, together with the full name and the
+      respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+     
+      Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m
+      tall.
+*/
+
+// 1. Tạo object chứa thông tin của Mark và John
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+// 2. Tính BMI cho Mark và John
+mark.calcBMI();
+john.calcBMI();
+
+// 3. So sánh BMI của Mark và John
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi.toFixed(2)}) is higher than ${
+      john.fullName
+    }'s (${john.bmi.toFixed(2)})!`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi.toFixed(2)}) is higher than ${
+      mark.fullName
+    }'s (${mark.bmi.toFixed(2)})!`
+  );
+} else {
+  console.log("They have the same BMI");
+}
